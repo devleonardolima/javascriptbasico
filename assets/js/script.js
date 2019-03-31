1f245ce06ae84ecbc9a4d9acbb5b7c31d9489d8d
= <devleonardo@hotmail.com>
2019-03-31 16:09:37 -0300

    Agora o projetinho começou a ficar um pouco bruto

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,30 @@ Como você pode ver !!
 
 //var nome = "Camila";
 var nome = "Leonardo";
+
+function Redirecionar(){
+    window.location.href = 'https://alunos.b7web.com.br/';
+}
+
+function MudarNome(){
+    var antigonome = nome;
+    var novonome = prompt("Qual é o seu nome agora ?");
+
+    if(novonome == ''){
+        while(novonome == ''){
+            novonome = prompt("Por favor qual é o seu nome agora ?");
+            if(novonome == null){
+                alert("O seu nome não foi modificado");
+                document.getElementById('area').innerHTML = antigonome;
+            }else if(nome != '' && nome != null){
+                document.getElementById('area').innerHTML = novonome;
+            }
+        }
+    }else if(novonome == null){
+        alert("O seu nome não foi modificado");
+        document.getElementById('area').innerHTML = antigonome;
+    }else{
+        document.getElementById('area').innerHTML = novonome;
+        nome = novonome;
+    }
+}
